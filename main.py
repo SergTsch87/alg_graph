72b6f62b7f7f9725c7a48954c67e11bd78b15a05
SergTsch87 <sergtsch87@gmail.com>
2025-08-06 15:34:52 +0300

    Add: Added another step to graph traversal in main(), main.py

--- main.py
+++ main.py
@@ -36,17 +36,34 @@ def main():
 
     visited_nodes_list = []
     queue_fifo_list = []
-    current_pointer = ""
+    current_vertex = ""
 
     # Initial
-    current_pointer = "A"
-    visited_nodes_list.append(current_pointer)
+    current_vertex = "A"
+    visited_nodes_list.append(current_vertex)
 
-    for item in graph_dict[current_pointer][::-1]:
+    # Added all neighbors of current_vertex vertex
+    for item in graph_dict[current_vertex][::-1]:
         queue_fifo_list.append(item)
 
+    print(f'visited_nodes_list == {visited_nodes_list}')
     print(f'queue_fifo_list == {queue_fifo_list}')
+    print(f'current_vertex == {current_vertex}')
 
+
+    # Next step
+    current_vertex = graph_dict[current_vertex][0]
+    visited_nodes_list.append(current_vertex)
+    queue_fifo_list = queue_fifo_list[:-1]
+
+    # Added all neighbors of current_vertex vertex
+    for item in graph_dict[current_vertex][::-1]:
+        queue_fifo_list.append(item)
+
+    print(f'visited_nodes_list == {visited_nodes_list}')
+    print(f'queue_fifo_list == {queue_fifo_list}')
+    print(f'current_vertex == {current_vertex}')
+    
 
 if __name__ == '__main__':
     main()
