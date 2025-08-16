016863d721ba8812ec3c142022a845b4c24d85f2
SergTsch87 <sergtsch87@gmail.com>
2025-08-16 13:47:45 +0300

    Red: Upd bfs(). Passed only test_single_node

--- graph_utils.py
+++ graph_utils.py
@@ -13,13 +13,25 @@ def bfs(graph_dict, start_vertex):
     if len(graph_dict) <= 1:
         if start_vertex in graph_dict:
             return [start_vertex]
-    
-    visited_nodes = [start_vertex]
-    queue_fifo = get_list_childs(start_vertex, graph_dict)
-    queue_fifo.reverse()
 
-    while queue_fifo:
-        start_vertex = queue_fifo.pop()
-        visited_nodes.append(start_vertex)
+    queue_fifo = []
+    visited_nodes = []
+
+    queue_fifo = [start_vertex]
+    visited_nodes = [start_vertex]
+    current_vertex = queue_fifo.pop(0)
+    neighbors_list = get_list_childs(current_vertex, graph_dict)
+
+    # visited_nodes = [start_vertex]
+    # queue_fifo = get_list_childs(start_vertex, graph_dict)
+    # queue_fifo.reverse()
+
+    # while queue_fifo:
+    #     start_vertex = queue_fifo.pop()
+    #     visited_nodes.append(start_vertex)
+        
+    #     temp_queue_fifo = get_list_childs(start_vertex, graph_dict)
+    #     temp_queue_fifo.reverse()
+    #     queue_fifo.append(temp_queue_fifo)
 
     return visited_nodes
