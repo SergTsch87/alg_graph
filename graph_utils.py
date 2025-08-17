2dcaaac8167dbb228d028133a9b801b8ee23529f
SergTsch87 <sergtsch87@gmail.com>
2025-08-18 01:30:04 +0300

    Green: queue_fifo to make deque obj in bfs(). Passed all tests

--- graph_utils.py
+++ graph_utils.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 # Повертає список батьків вершини vertex_parent
 def get_list_parents(vertex_parent, graph_dict):
     nodes_list = []
@@ -7,15 +10,37 @@ def get_list_parents(vertex_parent, graph_dict):
     return nodes_list
 
 
+# # bfs without addition modules
+# def bfs(graph_dict, start_vertex):
+#     if start_vertex not in graph_dict:
+#         return []
+    
+#     queue_fifo = [start_vertex]
+#     visited_nodes = [start_vertex]
+
+#     while queue_fifo:
+#         current_vertex = queue_fifo.pop(0)
+#         if graph_dict[current_vertex] != []: # to check a directed graph
+#             neighbors_list = get_list_parents(current_vertex, graph_dict)
+#             for item in neighbors_list:
+#                 if item not in visited_nodes:
+#                     queue_fifo.append(item)
+#                     visited_nodes.append(item)        
+
+#     return visited_nodes
+
+
+# bfs with deque module
 def bfs(graph_dict, start_vertex):
     if start_vertex not in graph_dict:
         return []
     
-    queue_fifo = [start_vertex]
+    queue_fifo = deque([start_vertex])
     visited_nodes = [start_vertex]
+    # queue = deque()
 
     while queue_fifo:
-        current_vertex = queue_fifo.pop(0)
+        current_vertex = queue_fifo.popleft() # fst
         if graph_dict[current_vertex] != []: # to check a directed graph
             neighbors_list = get_list_parents(current_vertex, graph_dict)
             for item in neighbors_list:
