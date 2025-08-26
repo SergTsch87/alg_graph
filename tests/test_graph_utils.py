386c0a91b7a1fafb7246cd868a1ff317c4d1d52d
SergTsch87 <sergtsch87@gmail.com>
2025-08-27 01:53:40 +0300

    Red: Add new test and bfs_with_parents(). New test not passed

--- tests/test_graph_utils.py
+++ tests/test_graph_utils.py
@@ -2,16 +2,6 @@ import pytest
 from graph_utils import bfs#, dfs
 
 
-# def test_parents_single_node():
-#     graph_dict = {
-#         "A": []
-#     }
-
-#     assert bfs(graph_dict, "A") == ["A"], ["A"]
-#     # assert dfs(graph_dict, "A") == ["A"]
-
-
-
 def test_single_node():
     graph_dict = {
         "A": []
