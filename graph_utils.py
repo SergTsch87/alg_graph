f313e630fa0da581d0c350558060728339a9cd29
SergTsch87 <sergtsch87@gmail.com>
2025-09-08 21:55:31 +0300

    Green: Upd dijkstras_alg() in graph_utils.py(). All tests passed

--- graph_utils.py
+++ graph_utils.py
@@ -144,10 +144,9 @@ def build_few_paths(parents, target):
 
 # Dijkstra's algorithm ( for undirected weighted graphs ) with deque module
 def dijkstras_alg(graph_dict, start_vertex): #, distance):
-    pass
-    # if len(graph_dict) == 1:
-    #     distance = 0
-    #     return {start_vertex: distance}
+    if len(graph_dict) == 1:
+        distance = 0
+        return {start_vertex: distance}
     
     # # curr_node = start_vertex
     # distance = [0, 'inf']
